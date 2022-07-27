8e42ab524cbaeca7c67c2e4fd8d4fc0e0822504c
camila <camilafkapp@gmail.com>
2022-07-27 03:01:03 -0300

    functioning

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,9 @@
-import express from 'express'
+import express from 'express';
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 export default app;
--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 import app from './app.js'
 import {sequelize} from './database/database.js'
+
 import './models/Characters.js'
+import './models/Comics.js'
 
 async function server() {
     try {
-        await sequelize.sync({force: true})
+        await sequelize.sync({force: true}) // Syncronizes the database with the models
         app.listen(3000)
         console.log('Server running on port 3000')
       } catch (error) {
@@ -13,4 +15,4 @@ async function server() {
 
 }
 
-server()
+server();
