d0871af35d60a023c27855d09827027c429873a7
camila <camilafkapp@gmail.com>
2022-07-27 03:00:18 -0300

    database connected

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,12 +1,14 @@
 import Sequelize from 'sequelize'
-import {Comics} from '../models/Comics.js'
-import {Character} from '../models/Characters.js'
 
-
-export const sequelize = new Sequelize('marvel', 'postgres', 'hausofgaga', {
+export const sequelize = new Sequelize(
+    'marvel',
+    'postgres',
+    'hausofgaga', 
+    {
     host: 'localhost',
     dialect: 'postgres',
-})
+  }
+);
 
 // Character.belongsToMany(Comic, {through: 'CharComics'})
 // Comic.belongsToMany(Character, {through: 'CharComics'})
