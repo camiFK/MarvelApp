d953c8f321cea83a25252e3bd8d42ffd8296f934
camila <camilafkapp@gmail.com>
2022-07-30 21:12:49 -0300

    fixing

--- client/src/redux/reducers/indexReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { rootReducer } from "./rootReducer";
-import { sortingReducer } from "./sortingReducer";
-
-export const reducers = combineReducers({
-    rootReducer,
-    sortingReducer,
-})
