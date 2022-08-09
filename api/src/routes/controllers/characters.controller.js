576cb0c764bcc4aedc13a3ea4507d02b0a80a579
camila <camilafkapp@gmail.com>
2022-08-09 13:41:49 -0300

    updates api

--- api/src/routes/controllers/characters.controller.js
+++ api/src/routes/controllers/characters.controller.js
@@ -5,7 +5,7 @@ import { Comic } from '../../models/Comics.js'
 export const getApiCharacters = async () => {
   try {
 
-    const response = await axios.get('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=7d4a743cdb10f022dbc5f5f4cb22040c&hash=8dec0b1007c4c461bdec676eb458aeeb&limit=60')
+    const response = await axios.get('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=7d4a743cdb10f022dbc5f5f4cb22040c&hash=8dec0b1007c4c461bdec676eb458aeeb&limit=100')
     
     const information = response.data.data.results.map(character => {
       return {
