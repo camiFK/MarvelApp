f393afce6f943872c5d54c5a6a6baf7a5aa5bb83
camila <camilafkapp@gmail.com>
2022-08-16 14:15:01 -0300

    preparing for deploy

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,14 +1,15 @@
 import Sequelize from 'sequelize'
+import 'dotenv/config'
+const { DATABASE_URL } = process.env
 
-export const sequelize = new Sequelize(
-    'marvel',
-    'postgres',
-    'hausofgaga', 
-    {
-    host: 'localhost',
-    dialect: 'postgres',
+export const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialect: "postgres",
+  dialectOptions: {
+    ssl : {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
-
-// Character.belongsToMany(Comic, {through: 'CharComics'})
-// Comic.belongsToMany(Character, {through: 'CharComics'})
+});
--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,13 @@
 import app from './app.js'
 import {sequelize} from './database/database.js'
+const {PORT} = process.env
+
 
 async function server() {
     try {
         await sequelize.sync({force: true}) // Syncronizes the database with the models
-        app.listen(3000)
-        console.log('Server running on port 3000')
+        app.listen(PORT)
+        console.log(`Server running on port ${PORT}`)
       } catch (error) {
         console.error('Unable to connect to the database:', error);
       }
